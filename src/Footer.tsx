a7e5b60cd9e1445c1e35a17af57804f8c1d5e9cf
JairoSainz <jairosainzrincon@gmail.com>
2024-11-28 22:22:33 -0700

    cambio en el footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ function Footer(){
         <div className="container mx-auto text-center">
             <h1 className="mb-4">© 2024 Sn.Music. Todos los derechos reservados.</h1>
             <div className="flex justify-center space-x-6">
-                <a href="/acerca" className="hover:underline hover:text-orange-500">Acerca de</a>
+                <a href="/acerca" className="hover:underline hover:text-orange-500">Acerca de Jairo</a>
                 <a href="/contacto" className="hover:underline hover:text-orange-500">Contacto</a>
                 <a href="/terminos" className="hover:underline hover:text-orange-500">Términos del Servicio</a>
                 <a href="/politica-privacidad" className="hover:underline hover:text-orange-500">Política de Privacidad</a>
